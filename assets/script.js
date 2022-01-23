069bbc597a1f92a9ddcf1a95ca2f0640c37c2f52
lizbett <rlizbett21@gmail.com>
2022-01-22 23:06:45 -0600

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-var textEl = document.getElementById('9am');
-var button = document.getElementById('saveBtn');
-var time = ['', '', '', '', '', '', '', ''];
-var text = [document.getElementById('9am')];
+document.getElementById('currentDay').textContent = moment().format("MMM Do YY");
+var nineAM = document.getElementById("9am");
+var tenAM = document.getElementById("10am");
+var elevenAM = document.getElementById("11am");
+var twelvePM = document.getElementById("12pm");
+var onePM = document.getElementById("1pm");
+var twoPM = document.getElementById("2pm");
+var threePM = document.getElementById("3pm");
+var fourPM = document.getElementById("4pm");
+var fivePM = document.getElementById("5pm");
 
-
-function save() {
-    console.log(texts[0].value);
-    localStorage.setItem('nineAM', save);
-}
-button.addEventListener('click', save);
 
